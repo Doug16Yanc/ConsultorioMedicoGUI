a192cc1bcb20ab6f4e74df18bfe528de10c1b92b
Douglas Holanda <129301271+Doug16Yanc@users.noreply.github.com>
2024-10-02 19:51:25 -0400

    Update DatabaseConnection.java

--- src/repository/DatabaseConnection.java
+++ src/repository/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/healthcare";
     private static final String USER = "root";
-    private static final String PASSWORD = 123";
+    private static final String PASSWORD = "123";
 
     public static Connection getConnection() {
         Connection connection = null;
